77c40420f22aaab8410aafc4a6a036ea2fe8179f
James <james@jamesnzl.xyz>
2022-07-11 17:44:56 +1200

    refactor: :recycle: refactor into individual replacer 'modules'

--- lib/format-commit-messages.ts
+++ lib/format-commit-messages.ts
@@ -36,28 +36,39 @@ function hasCommits(body: NextApiRequest['body']): body is HasCommits {
   return (body && 'commits' in body);
 }
 
-const MAX_DISCORD_COMMIT_MESSAGE_LENGTH = 50;
+/**
+ * Fixes unclosed inline code backticks after Discord message truncation.
+ */
+function fixTruncatedInlineCode(message: string) {
+  const MAX_DISCORD_COMMIT_MESSAGE_LENGTH = 50;
+
+  const discordTruncatedMessage = message.slice(0, MAX_DISCORD_COMMIT_MESSAGE_LENGTH);
+  // missing opening backtick if uneven count
+  const isMissingClosingBacktick = Boolean(((discordTruncatedMessage.match(/`/g) ?? []).length) % 2);
+
+  if (!isMissingClosingBacktick) return message;
+
+  // add ellipsis and close the last backtick
+  return message.replace(/(.{4})$/, '...``$1');
+}
+
+/**
+ * Resolves recognised gitmojis codes to their emoji characters, to minimise Discord truncation.
+ */
+function resolveGitmojiToEmoji(message: string) {
+  // resolve gitmoji codes to the emoji itself (reduce characters)
+  const gitmojiCode = message.match(/(:[\w_]+:)/)?.[1];
+  if (!gitmojiCode) return message;
+
+  return message.replace(gitmojiCode, resolveGitmojiCode(gitmojiCode));
+}
 
 export function formatCommitMessages(body: NextApiRequest['body']) {
   if (!hasCommits(body)) return body;
 
   body.commits.map(commit => {
-    // remove inline code backticks only if cutoff
-    const discordTruncatedMessage = commit.message.slice(0, MAX_DISCORD_COMMIT_MESSAGE_LENGTH);
-    // missing opening backtick if uneven count
-    const isMissingClosingBacktick = Boolean(((discordTruncatedMessage.match(/`/g) ?? []).length) % 2);
-
-    if (isMissingClosingBacktick) {
-      // add ellipsis and close the last backtick
-      commit.message = commit.message.replace(/(.{4})$/, '...``$1');
-    }
-
-    // resolve gitmoji codes to the emoji itself (reduce characters)
-    const gitmojiCode = commit.message.match(/(:[\w_]+:)/)?.[1];
-    if (!gitmojiCode) return commit;
-
-    commit.message = commit.message.replace(gitmojiCode, resolveGitmojiCode(gitmojiCode));
-
+    commit.message = fixTruncatedInlineCode(commit.message);
+    commit.message = resolveGitmojiToEmoji(commit.message);
     return commit;
   });
 
