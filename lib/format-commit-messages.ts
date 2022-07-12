2d0e42551b500544ebe917224b108400d79d582b
James <james@jamesnzl.xyz>
2022-07-12 16:10:29 +1200

    test: :test_tube: add tests for `fixTruncatedInlineCode()`

--- lib/format-commit-messages.ts
+++ lib/format-commit-messages.ts
@@ -91,6 +91,91 @@ export function formatCommitMessages(body: NextApiRequest['body']) {
 
 if (import.meta.vitest) {
   const { describe, it, expect } = import.meta.vitest;
+  describe('#fixTruncatedInlineCode', () => {
+    it('ignores short commits', () => {
+      expect(fixTruncatedInlineCode('feat: short commit')).toBe('feat: short commit');
+      expect(fixTruncatedInlineCode('feat: :technologist: `short inline`')).toBe('feat: :technologist: `short inline`');
+    });
+
+    it('ignores long commits with no inline code', () => {
+      expect(fixTruncatedInlineCode('feat: long commit'.repeat(20))).toBe('feat: long commit'.repeat(20));
+    });
+
+    function addBackticks(string: string) {
+      return '`' + string + '`';
+    }
+
+    it.each(
+      [
+        // total length 46, not truncated by Discord
+        [addBackticks('X'.repeat(44)), addBackticks('X'.repeat(44))],
+        // total length 47, not truncated by Discord
+        [addBackticks('X'.repeat(45)), addBackticks('X'.repeat(45))],
+        // total length 48, not truncated by Discord
+        [addBackticks('X'.repeat(46)), addBackticks('X'.repeat(46))],
+        // total length 49, not truncated by Discord
+        [addBackticks('X'.repeat(47)), addBackticks('X'.repeat(47))],
+        // total length 50, not truncated by Discord
+        [addBackticks('X'.repeat(48)), addBackticks('X'.repeat(48))],
+        // total length 51, truncated by Discord
+        [addBackticks('X'.repeat(49)), addBackticks('X'.repeat(45)) + addBackticks('X'.repeat(4))],
+        // total length 52, truncated by Discord
+        [addBackticks('X'.repeat(50)), addBackticks('X'.repeat(45)) + addBackticks('X'.repeat(5))],
+        // total length 53, truncated by Discord
+        [addBackticks('X'.repeat(51)), addBackticks('X'.repeat(45)) + addBackticks('X'.repeat(6))],
+        // total length 54, truncated by Discord
+        [addBackticks('X'.repeat(52)), addBackticks('X'.repeat(45)) + addBackticks('X'.repeat(7))],
+      ],
+    )('correctly appends truncated backticks', (message, expected) => {
+      const fixed = fixTruncatedInlineCode(message);
+
+      expect(fixed).toBe(expected);
+
+      // must not contain ``
+      // TODO: add ZWSP between inserted closing/opening
+      // expect(fixed).not.toContain('``');
+
+      // must have an even number of backticks in the first 50 characters
+      const isMissingClosingBacktick = Boolean(((fixed.match(/`/g) ?? []).length) % 2);
+      expect(isMissingClosingBacktick).toBe(false);
+    });
+
+    it.each(
+      [
+        // <42>`<2>` = total length 46, not truncated
+        ['X'.repeat(42) + addBackticks('X'.repeat(2)), 'X'.repeat(42) + addBackticks('X'.repeat(2))],
+        // <43>`<2>` = total length 47 not truncated
+        ['X'.repeat(43) + addBackticks('X'.repeat(2)), 'X'.repeat(43) + addBackticks('X'.repeat(2))],
+        // <44>`<2>` = total length 48, not truncated
+        ['X'.repeat(44) + addBackticks('X'.repeat(2)), 'X'.repeat(44) + addBackticks('X'.repeat(2))],
+        // <45>`<2>` = total length 49, not truncated
+        ['X'.repeat(45) + addBackticks('X'.repeat(2)), 'X'.repeat(45) + addBackticks('X'.repeat(2))],
+        // <46>`<2>` = total length 50, not truncated
+        ['X'.repeat(46) + addBackticks('X'.repeat(2)), 'X'.repeat(46) + addBackticks('X'.repeat(2))],
+        // <47>`<2>` = total length 51, truncated <47>`XX|` => <47>`X`|`X`
+        ['X'.repeat(47) + addBackticks('X'.repeat(2)), 'X'.repeat(47) + addBackticks('X') + addBackticks('X')],
+        // <48>`<2>` = total length 52, truncated <48>`X|X` => <48>  |`XX`
+        ['X'.repeat(48) + addBackticks('X'.repeat(2)), 'X'.repeat(48) + '  ' + addBackticks('XX')],
+        // <49>`<2>` = total length 53, truncated <49>`|XX` => <49> |`XX`
+        ['X'.repeat(49) + addBackticks('X'.repeat(2)), 'X'.repeat(49) + ' ' + addBackticks('XX')],
+        // <50>`<2>` = total length 54, truncated <50>|`XX` => <50>|`XX`
+        ['X'.repeat(50) + addBackticks('X'.repeat(2)), 'X'.repeat(50) + addBackticks('XX')],
+      ],
+    )('correctly truncates opening backticks', (message, expected) => {
+      const fixed = fixTruncatedInlineCode(message);
+
+      expect(fixed).toBe(expected);
+
+      // must not contain ``
+      // TODO: add ZWSP between inserted closing/opening
+      // expect(fixed).not.toContain('``');
+
+      // must have an even number of backticks in the first 50 characters
+      const isMissingClosingBacktick = Boolean(((fixed.match(/`/g) ?? []).length) % 2);
+      expect(isMissingClosingBacktick).toBe(false);
+    });
+  });
+
   describe('#resolveGitmojiToEmoji', () => {
     it('resolves :technologist:', () => {
       expect(resolveGitmojiToEmoji('feat: :technologist: remove file')).toContain('🧑‍💻');
