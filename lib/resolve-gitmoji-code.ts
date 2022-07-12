1a495d43a8a7192efff4b58aa7a938f8355d748a
James <james@jamesnzl.xyz>
2022-07-12 14:47:01 +1200

    test: :white_check_mark: add tests for `resolveGitmojiCode()`

--- lib/resolve-gitmoji-code.ts
+++ lib/resolve-gitmoji-code.ts
@@ -6,3 +6,24 @@ export function resolveGitmojiCode(code: string) {
 
   return resolvedGitmoji.emoji;
 }
+
+if (import.meta.vitest) {
+  const { describe, it, expect } = import.meta.vitest;
+  describe('#resolveGitmojiCode', () => {
+    it('resolves :technologist:', () => {
+      expect(resolveGitmojiCode(':technologist:')).toBe('🧑‍💻');
+    });
+    it('resolves :art:', () => {
+      expect(resolveGitmojiCode(':art:')).toBe('🎨');
+    });
+    it('resolves :fire:', () => {
+      expect(resolveGitmojiCode(':fire:')).toBe('🔥');
+    });
+    it('doesn\'t resolve :shrug:', () => {
+      expect(resolveGitmojiCode(':shrug:')).toBe(':shrug:');
+    });
+    it('doesn\'t resolve feat: :fire: remove file', () => {
+      expect(resolveGitmojiCode('feat: :fire: remove file')).toBe('feat: :fire: remove file');
+    });
+  });
+}
